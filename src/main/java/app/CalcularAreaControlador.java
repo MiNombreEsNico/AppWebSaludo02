760c70040b27ffe0a4c976fdd42552ce54db5330
Miguel Nicolás Basualdo Martinez <niicomartiinez22@gmail.com>
2025-06-14 15:27:48 -0300

    html changes & css added

--- src/main/java/app/CalcularAreaControlador.java
+++ src/main/java/app/CalcularAreaControlador.java
@@ -10,36 +10,35 @@ import modelo.TrianguloRectangulo;
 @RestController
 public class CalcularAreaControlador {
         
-	public CalcularAreaControlador() {
-		// TODO Auto-generated constructor stub
-	}
-	private Rectangulo rectangulo = null;
-	private TrianguloRectangulo triangulo = null;
-	
-	@GetMapping("/area")
-	public String area(@RequestParam(value = "altura", defaultValue = "5") String altura,
-					   @RequestParam(value = "base", defaultValue = "10") String base) {
-		
-		Float alturaFigura = Float.parseFloat(altura);
-		Float baseFigura = Float.parseFloat(base);
-		
-		rectangulo = new Rectangulo();
-		
-		rectangulo.setAltura(alturaFigura);
-		
-		rectangulo.setBase(baseFigura);
-		
-		Float areaRectangulo = rectangulo.calcularArea();
-		
-		triangulo = new TrianguloRectangulo();
-		
-		triangulo.setAltura(alturaFigura);
-		
-		triangulo.setBase(baseFigura);
-		
-		Float areaTriangulo = triangulo.calcularArea();
-		
-		return "<p><b>Area rectángulo=, </p></b>" + areaRectangulo.toString() + "<br>"
-				+ "<p><b>Area triángulo rectángulo=, </p></b>" + areaTriangulo.toString() + "";
-	}
+    public CalcularAreaControlador() {
+        // Constructor por defecto
+    }
+    private Rectangulo rectangulo = null;
+    private TrianguloRectangulo triangulo = null;
+    
+    @GetMapping("/area")
+    public String area(@RequestParam(value = "altura", defaultValue = "5") String altura,
+                       @RequestParam(value = "base", defaultValue = "10") String base) {
+        
+        Float alturaFigura = Float.parseFloat(altura);
+        Float baseFigura = Float.parseFloat(base);
+        
+        rectangulo = new Rectangulo();
+        rectangulo.setAltura(alturaFigura);
+        rectangulo.setBase(baseFigura);
+        Float areaRectangulo = rectangulo.calcularArea();
+        
+        triangulo = new TrianguloRectangulo();
+        triangulo.setAltura(alturaFigura);
+        triangulo.setBase(baseFigura);
+        Float areaTriangulo = triangulo.calcularArea();
+        
+        return "<div class='md-card-content'>" +
+               "<span class='md-title'>Resultados</span>" +
+               "<ul class='md-list'>" +
+               "<li><b>Área rectángulo:</b> " + areaRectangulo + "</li>" +
+               "<li><b>Área triángulo rectángulo:</b> " + areaTriangulo + "</li>" +
+               "</ul>" +
+               "</div>";
+    }
 }
--- src/main/java/app/HolaMundoControlador.java
+++ src/main/java/app/HolaMundoControlador.java
@@ -1,27 +1,17 @@
 package app;
 
-import java.util.Date;
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HolaMundoControlador {
-    private static final String respuesta = "Hola, %s!";
-    private final AtomicLong contador = new AtomicLong();
-        
-	public HolaMundoControlador() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	@GetMapping("/holamundo")
-	public String holamundo(@RequestParam(value = "nombre", defaultValue = "Mundo") String nombre) {
-	    //String respuestaJSON = String.format(respuesta, nombre);
-	    //respuestaJSON = respuestaJSON + new Date().toString();
-	    //HolaMundo objetoJSON = new HolaMundo(contador.incrementAndGet(), respuestaJSON);
-	    //return objetoJSON;
-		return "<p><b>Hola, </p></b>" + nombre + "! ...";
-	}
+
+    @GetMapping("/holamundo")
+    public String holamundo(@RequestParam(value = "nombre", defaultValue = "Mundo") String nombre) {
+        return "<div class='md-card-content'>" +
+               "<span class='md-title'>¡Hola, " + nombre + "!</span>" +
+               "<p class='md-text'>¡Bienvenido/a a la aplicación de cálculo de áreas!</p>" +
+               "</div>";
+    }
 }
